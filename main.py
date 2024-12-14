e035890c17cb386cf663f00aac024bab31ec3e17
nctqt <noctourniquet@protonmail.com>
2024-12-14 02:28:02 -0800

    triangle working

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting asteroids!")
@@ -12,6 +13,11 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    delta_ms = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 20)
 
     while True:
         for event in pygame.event.get():
@@ -19,9 +25,11 @@ def main():
                 return
         pygame.Surface.fill(screen, 0)
 
-
+        player.draw(screen)
 
         pygame.display.flip()
+        delta_ms = clock.tick(60)
+        dt = delta_ms / 1000
 
 if __name__ == "__main__":
     main()
